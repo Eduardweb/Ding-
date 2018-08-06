8b561de1780c8ab6020bbdd13088a6262c799558
Eduard <o.eduardweb@gmail.com>
2018-08-07 00:29:36 +0300

    start mobile

---
+++ js/custom.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+// Slick slider http://kenwheeler.github.io/slick/
+    $('.benefits-slider').slick({
+        dots: false,
+        infinite: false,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          // {
+          //   breakpoint: 1919,
+          //   settings: {
+          //     slidesToShow: 5,
+          //     slidesToScroll: 1,
+          //     infinite: true
+          //   }
+          // }
+        ]
+    });
+
+    $('a[data-slide]').click(function(e) {
+      $('.benefits-nav a').removeClass();
+      $(this).addClass('active-slider');
+      e.preventDefault();
+      var slideno = $(this).data('slide');
+      $('.slider-nav').slick('slickGoTo', slideno - 1);
+    });  
+// 
+
+    $('.ding-slider').slick({
+      dots: false,
+      infinite: false,
+      arrows: false,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1919,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 1198,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 990,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 340,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+      ]
+    });
+
+
+});
